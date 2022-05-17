d63def5d6cd6c121343ba663dbb34fd79fb76dd1
xxxw1tnessbtwxxx <89967499+xxxw1tnessbtwxxx@users.noreply.github.com>
2022-05-18 00:38:14 +0700

    Update jquery.countdown.js

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -1,19 +1,11 @@
-/**
- * @name		jQuery Countdown Plugin
- * @author		Martin Angelov
- * @version 	1.0
- * @url			http://tutorialzine.com/2011/12/countdown-jquery/
- * @license		MIT License
- */
-
 (function($){
 	
-	// Number of seconds in every time division
+
 	var days	= 24*60*60,
 		hours	= 60*60,
 		minutes	= 60;
 	
-	// Creating the plugin
+	
 	$.fn.countdown = function(prop){
 		
 		var options = $.extend({
@@ -23,47 +15,47 @@
 		
 		var left, d, h, m, s, positions;
 
-		// Initialize the plugin
+		
 		init(this, options);
 		
 		positions = this.find('.position');
 		
 		(function tick(){
 			
-			// Time left
+			
 			left = Math.floor((options.timestamp - (new Date())) / 1000);
 			
 			if(left < 0){
 				left = 0;
 			}
 			
-			// Number of days left
+			
 			d = Math.floor(left / days);
 			updateDuo(0, 1, d);
 			left -= d*days;
 			
-			// Number of hours left
+			
 			h = Math.floor(left / hours);
 			updateDuo(2, 3, h);
 			left -= h*hours;
 			
-			// Number of minutes left
+			
 			m = Math.floor(left / minutes);
 			updateDuo(4, 5, m);
 			left -= m*minutes;
 			
-			// Number of seconds left
+			
 			s = left;
 			updateDuo(6, 7, s);
 			
-			// Calling an optional user supplied callback
+			
 			options.callback(d, h, m, s);
 			
-			// Scheduling another call of this function in 1s
+			
 			setTimeout(tick, 1000);
 		})();
 		
-		// This function updates two digit positions at once
+		
 		function updateDuo(minor,major,value){
 			switchDigit(positions.eq(minor),Math.floor(value/10)%10);
 			switchDigit(positions.eq(major),value%10);
@@ -76,7 +68,7 @@
 	function init(elem, options){
 		elem.addClass('countdownHolder');
 
-		// Creating the markup inside the container
+		
 		$.each(['Days','Hours','Minutes','Seconds'],function(i){
 			$('<span class="count'+this+'">').html(
 				'<span class="position">\
@@ -94,7 +86,7 @@
 
 	}
 
-	// Creates an animated transition between the two numbers
+	
 	function switchDigit(position,number){
 		
 		var digit = position.find('.digit')
@@ -104,7 +96,7 @@
 		}
 		
 		if(position.data('digit') == number){
-			// We are already showing this number
+			
 			return false;
 		}
 		
@@ -119,8 +111,7 @@
 			html:number
 		});
 		
-		// The .static class is added when the animation
-		// completes. This makes it run smoother.
+		
 		
 		digit
 			.before(replacement)
